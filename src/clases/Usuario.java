0429fb87e970d40f4768438fd1b3350a9be2d216
dv237 <dv237@LAPTOP-NP2GCT2O>
2021-02-27 01:51:38 -0500

    Finalizado

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
