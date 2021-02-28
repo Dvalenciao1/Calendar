2ca3fe4f891a6b72d0d34c3685b2b6d180feaca6
dv237 <dv237@LAPTOP-NP2GCT2O>
2021-02-28 00:19:12 -0500

    Finalizado

--- src/clases/SQL.java
+++ src/clases/SQL.java
@@ -8,16 +8,21 @@ import java.util.logging.Logger;
 
 public class SQL {
 
-    private final String db = "calendar";
-    private final String user = "root";
-    private final String pass = "root";
-    private final String url = "jdbc:mysql://localhost:3306/" + db;
+    private final String db = "sql10395611";
+    private final String user = "sql10395611";
+    private final String pass = "NEirVJAnmA";
+    private final String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/" + db;
+    
+    private final String db1 = "calendar";
+    private final String user1 = "root";
+    private final String pass1 = "root";
+    private final String url1 = "jdbc:mysql://localhost:3306/" + db1;
     private Connection con = null;
 
     public Connection getConexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url, user, pass);
+            con = DriverManager.getConnection(url1, user1, pass1);
         } catch (ClassNotFoundException e) {
             Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, e);
         } catch (SQLException e) {
